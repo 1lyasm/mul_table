1e8b69b393cf670959e9e7d78b73107a830e2aaa
1lyasm <il.mystafa+1@gmail.com>
2023-05-26 13:22:52 +0400

    fix sorting scores

--- src/main/java/org/mul_table/HighScoreTables.java
+++ src/main/java/org/mul_table/HighScoreTables.java
@@ -1,6 +1,7 @@
 package org.mul_table;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 
 public class HighScoreTables {
@@ -8,7 +9,8 @@ public class HighScoreTables {
     public void add_score(ExerciseStatistic statistic) {
         int e_index = -1;
         for (int i = 0; i < this.tables.size(); ++i)
-            if (this.tables.get(i).get_exercise().equals(statistic.get_e_mode()))
+            // TODO: enforce cant have exercise modes with duplicate names
+            if (this.tables.get(i).get_exercise().get_name().equals(statistic.get_e_mode().get_name()))
                 e_index = i;
         if (e_index == -1) {
             this.tables.add(new HighScoreTable(statistic.get_e_mode()));
@@ -16,11 +18,10 @@ public class HighScoreTables {
         }
         else {
             this.tables.get(e_index).get_scores().add(statistic);
-            // TODO: this does not sort
-            this.tables.get(e_index).get_scores().sort(new Comparator<ExerciseStatistic>() {
+            Collections.sort(this.tables.get(e_index).get_scores(), new Comparator<ExerciseStatistic>() {
                 @Override
                 public int compare(ExerciseStatistic s_1, ExerciseStatistic s_2) {
-                    return Double.compare(s_1.get_accuracy_score() + s_1.get_speed_score(), s_2.get_accuracy_score() + s_2.get_speed_score());
+                    return -Double.compare(s_1.get_accuracy_score() + s_1.get_speed_score(), s_2.get_accuracy_score() + s_2.get_speed_score());
                 }
             });
         }
